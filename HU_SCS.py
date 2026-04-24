af036ec266bd6cb8b734e71fc75d431040ee0b62
Felipe <felipegarcia.esp@gmail.com>
2026-04-24 15:30:22 -0400

    UPDATE CODE

--- HU_SCS.py
+++ HU_SCS.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def tc_california(Lcauce, RangoElev):
-    # TC California para cuencas montañosas, con área superior a 2 km2
+    # TC California para cuencas montañosas, con área superior a 2 km2 (CORROBORAR)
     # Lcauce = Largo del cauce principal (km)
     # RangoElev = ElevMax-ElevMin de la cuenca (m)
     # Resultado tc se expresa en horas
@@ -13,7 +13,7 @@ def tc_california(Lcauce, RangoElev):
     return tc
 
 def tc_USNavy(Lcauce, Vmed):
-    # Conocida también como tc del Texas Highway Department o método de la velocidad media
+    # Conocida también como tc del Texas Highway Department o método de la velocidad media (CORROBORAR)
     # Lcauce = Largo del cauce principal (km)
     # Vmed = Velocidad media del cauce principal (m/s)
     # Resultado tc se expresa en horas
@@ -22,3 +22,24 @@ def tc_USNavy(Lcauce, Vmed):
     else:
         tc = (1.0 / 3.6) * (Lcauce / Vmed)
     return tc
+
+def tc_Giandotti(Area, Lcauce, ElevMed, ElevMin):
+    # Tc Giandotti para cuencas con área entre 50 y 500 km2 (CORROBORAR)
+    # Area = Área de la cuenca (km2)
+    # Lcauce = Largo del cauce principal (km)
+    # ElevMed = Elevación media de la cuenca (m)
+    # ElevMin = Elevación mínima de la cuenca (m)
+    # Resultado tc se expresa en horas
+    if Area == 0 or Lcauce == 0 or ElevMed == 0 or ElevMin == 0:
+        tc = 0.0
+    else:
+        Hm = ElevMed - ElevMin
+        tc = (4.0 * (Area ** 0.5) + 1.5 * Lcauce) / (0.8 * (Hm ** 0.5))
+    return tc
+
+def tc_LagSCS(Lcauce, CN, S):
+    # Tc Lag SCS para cuencas agrícolas o urbanas con área inferior a 8 km2 (CORROBORAR)
+    # Lcauce = Largo del cauce principal (km)
+    # CN = Valor de curva número, humedad antecedente II (CORROBORAR)
+    # S = Pendiente media de la cuenca (m/m)
+    
